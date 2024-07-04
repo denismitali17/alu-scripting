dec991711cd6c1c8209ec8d416bab72cd717c041
denismitali17 <d.mitali1@alustudent.com>
2024-07-04 09:58:32 -0700

    How many subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
     try:
