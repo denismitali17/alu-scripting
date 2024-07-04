b898193d8e3aa08f2e2537ca6cd412bb5c5b1ca4
denismitali17 <d.mitali1@alustudent.com>
2024-07-04 09:49:15 -0700

    How many subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,18 +1,17 @@
-
 #!/usr/bin/python3
-"""Script that fetches the number of subscribers for a given subreddit."""
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for the given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data:
-            return data.get('subscribers', 0)
-    return 0
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
 
+    except Exception:
+        return 0
